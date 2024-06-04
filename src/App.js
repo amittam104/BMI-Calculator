ad95ed2ff52dfbb74b042bf4e7ff7623a3a2bb28
Amit Tambulkar <amittambulkar104@gmail.com>
2024-06-04 16:57:43 +0530

    chore: add markup for the app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
-import "./App.css";
-
 export default function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <h3>Enter your details below</h3>
+      <BmiForm />
+      <Result />
+    </div>
+  );
+}
+
+function BmiForm() {
+  return (
+    <form action="submit">
+      <div>
+        <label>Weight</label>
+        <input type="number" placeholder="kg" />
+      </div>
+      <div>
+        <label>Height</label>
+        <input type="number" placeholder="cm" />
+      </div>
+      <button>Calculate</button>
+    </form>
+  );
+}
+
+function Result() {
+  return (
+    <div>
+      <h4>Welcome</h4>
+      <p>Enter your height and weight to calculate your BMI</p>
+    </div>
+  );
 }
